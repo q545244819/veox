aabbdb578e959638c33f3fb396dc42d509e21466
johnhsm <545244819@qq.com>
2016-05-31 16:29:23 +0800

    Add duoshuo to Article.vue

--- template/_template/src/components/Article.vue
+++ template/_template/src/components/Article.vue
@@ -3,13 +3,21 @@
   <h1 v-else class="title">About Me!</h1>
   <p v-if="$route.path !== '/about'" class="date">Article Post Time: <span v-text="$route.params.date"></span></p>
   <div class="markdown-body" v-html="html"></div>
+  <!-- 多说评论框 start -->
+  <div class="ds-thread" data-thread-key="{{ duoshuo.key }}" data-title="{{ duoshuo.title }}" data-url="{{ duoshuo.url }}"></div>
+  <!-- 多说评论框 end -->
 </template>
 
 <script>
   export default {
     data() {
       return {
-        html: ''
+        html: '',
+        duoshuo: {
+          key: '',
+          title: '',
+          url: ''
+        }
       }
     },
     ready() {
@@ -25,6 +33,11 @@
         })
         .then(html => {
           this.html = html
+          this.duoshuo.key = $route.path !== '/about' ? `${params.date}/${params.title}` : 'about'
+          this.duoshuo.title = $route.path !== '/about' ? params.title : 'about'
+          this.duoshuo.url = location.href
+
+          DUOSHUO.EmbedThread(document.querySelector('.ds-thread'))
         })
         .catch(err => {
           console.error(err)
