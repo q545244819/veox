ed31c5bacb4b77a4923e06059fe65d3df7dca935
johnhsm <545244819@qq.com>
2016-05-31 23:02:51 +0800

    add config.js for template

--- template/_template/src/main.js
+++ template/_template/src/main.js
@@ -3,11 +3,19 @@ import VueRouter from 'vue-router'
 import VueAsyncData from 'vue-async-data'
 
 import App from './App.vue'
+import Config from './config'
 
 Vue.use(VueRouter)
 Vue.use(VueAsyncData)
 
 const router = new VueRouter()
+const duoshuoQuery = {short_name: Config.duoshuo}
+const ds = document.createElement('script')
+
+ds.type = 'text/javascript';ds.async = true
+ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js'
+ds.charset = 'UTF-8'
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds)
 
 router.map({
   '/articles': {
