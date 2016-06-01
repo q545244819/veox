75fba2ed8288ba37dffc77ae8280bcdaa949ec76
林军 <117280858@qq.com>
2016-06-01 22:41:33 +0800

    make a single Duoshuo.vue

---
+++ template/_template/src/components/Duoshuo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ds-thread" data-thread-key="{{ key }}" data-title="{{ title }}" data-url="{{ url }}"></div>
+</template>
+
+<script>
+  export default {
+    props: {
+      key: String,
+      title: String,
+      url: String
+    },
+    ready() {
+      try {
+        DUOSHUO.EmbedThread(document.querySelector('.ds-thread'))
+      } catch(e) {}
+    }
+  }
+</script>
